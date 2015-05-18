9866d96ad9f0b772345c43242e4856c76b9249dd
abeconnelly <abram@curoverse.com>
2015-05-18 17:22:51 -0400

    upgrading to oauth2

--- docker.io/config.php
+++ docker.io/config.php
@@ -3,4 +3,7 @@
 $gDbPassword = "shakespeare";
 $gBackendBaseDir = "/home/trait"; // (can omit if using default)
 
+$gOAuth2ClientID = '774153778747-151ivi668jjkovb2gmk8343rm2p911bc.apps.googleusercontent.com';
+$gOAuth2ClientSecret = 'UD-xmaZIRahycLN5dcgI9KQZ';
+
 ?>
